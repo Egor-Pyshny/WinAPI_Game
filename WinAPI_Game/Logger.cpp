dbdd1aebae2bab24dc5b17a62395cb343a15db2e
Egor251 <egorpyshny@gmail.com>
2023-12-05 19:22:10 +0300

    _

--- WinAPI_Game/Logger.cpp
+++ WinAPI_Game/Logger.cpp
@@ -1,1 +1,91 @@
 #include "Logger.h"
+
+Logger::Logger(int logger_type)
+{
+	this->logger_type = logger_type;
+}
+
+Logger::~Logger()
+{
+	if (this->started) {
+		this->started = false;
+		WaitForSingleObject(this->hLoggerThread, INFINITE);
+	}
+}
+
+void Logger::changeType(int logger_type)
+{
+	this->logger_type = logger_type;
+}
+
+bool Logger::start(LPVOID lpParam)
+{
+	started = true;
+	if (first_start) {
+		switch (this->logger_type) {
+		case ANGLES:
+		{
+			hLoggerThread = CreateThread(nullptr, 0, logAngles, lpParam, 0, nullptr);;
+			break;
+		}
+		case COORDS:
+		{
+			hLoggerThread = CreateThread(nullptr, 0, logCoordinates, lpParam, 0, nullptr);;
+			break;
+		}
+		case GAMEINFO:
+		{
+			hLoggerThread = CreateThread(nullptr, 0, logGameInfo, lpParam, 0, nullptr);;
+			break;
+		}
+		default:
+		{
+			started = false;
+			return false;
+		}
+		}
+		if (hLoggerThread == NULL) {
+			return false;
+		}
+		first_start = false;
+	}
+	return true;
+}
+
+void Logger::stop()
+{
+	this->log = false;
+}
+
+DWORD WINAPI logAngles(LPVOID lpParam)
+{
+	ofstream stream("");
+	while (started) {
+		if (log) {
+		
+		}
+	}
+	return 0;
+}
+
+DWORD WINAPI logCoordinates(LPVOID lpParam)
+{
+	ofstream stream("");
+	while (started) {
+		if (log) {
+
+		}
+	}
+	return 0;
+}
+
+DWORD WINAPI logGameInfo(LPVOID lpParam)
+{
+	ofstream stream("");
+	while (started) {
+		if (log) {
+
+		}
+	}
+	return 0;
+}
--- WinAPI_Game/Logger.h
+++ WinAPI_Game/Logger.h
@@ -1,27 +1,37 @@
 #pragma once
 
+#define ANGLES 1
+#define COORDS 2
+#define GAMEINFO 3
+
 #include <string>
 #include <fstream>
 #include <windows.h>
 
 using namespace std;
 
+DWORD WINAPI logAngles(LPVOID lpParam);
+DWORD WINAPI logCoordinates(LPVOID lpParam);
+DWORD WINAPI logGameInfo(LPVOID lpParam);
+
 class Logger
 {
 public:
-	Logger(string logger_type);
+	Logger(int logger_type);
 	~Logger();
-	DWORD WINAPI logAngles(LPVOID lpParam);
-	DWORD WINAPI logCoordinates(LPVOID lpParam);
-	DWORD WINAPI logGameInfo(LPVOID lpParam);
+	void changeType(int logger_type);
+	bool start(LPVOID lpParam);
+	void stop();
+
 private:
 	bool started = false;
+	bool first_start = true;
 	bool log = false;
-	HANDLE hLoggerThread;
+	HANDLE hLoggerThread = NULL;
 	ofstream stream;
+	int logger_type;
 	const string angles_file = "";
 	const string coordinates_file = "";
 	const string gameinfo_file = "";
-	string logger_type;
 };
 
